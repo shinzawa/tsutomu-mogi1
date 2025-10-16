580279a70b7fdb5825254de803d852e584a5d4aa
Tsutomu Shinzawa <tsutomu.shinzawa320@gmail.com>
2025-10-16 21:19:25 +0900

    ログイン画面のスタイル作成

---
+++ src/resources/views/auth/login.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/auth/login.css')}}">
+@endsection
+
+@section('link')
+@endsection
+
+@section('content')
+<div class="login-form">
+    <h2 class="login-form__heading content__heading">ログイン</h2>
+    <div class="login-form__inner">
+        <div>
+            <form class="login-form__form" action="/login" method="post">
+                @csrf
+                <div class="login-form__group">
+                    <label class="login-form__label" for="email">メールアドレス</label>
+                    <input class="login-form__input" type="mail" name="email" id="email">
+                    <p class="register-form__error-message">
+                        @error('email')
+                        {{ $message }}
+                        @enderror
+                    </p>
+                </div>
+                <div class="login-form__group">
+                    <label class="login-form__label" for="password">パスワード</label>
+                    <input class="login-form__input" type="password" name="password" id="password">
+                    <p>
+                        @error('password')
+                        {{ $message }}
+                        @enderror
+                    </p>
+                </div>
+                <button class="login-form__btn btn" type="submit" value="ログイン">ログインする</button>
+            </form>
+            <div class="login__nav-href">
+                <a href="/register" class="login__nav">会員登録はこちら</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection('content')
